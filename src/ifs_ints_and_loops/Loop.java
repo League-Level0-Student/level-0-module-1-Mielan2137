f8551c1f11e984c33a04ec5570d427c8103fde14
ilabstaff <ilabstaff@172.16.116.71>
2019-02-06 19:29:08 -0800

    This is my latest Riddler (Feb. 06, 2019)

---
+++ src/ifs_ints_and_loops/Loop.java
@@ -0,0 +1,14 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Loop {
+	public static void main(String[] args) {
+		for (int age=17;age>=0;age--){
+		System.out.println(age);
+			
+		}
+		
+	}
+}
+
